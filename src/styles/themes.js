1e24f9bc6b4e5d789d237c93347ed7ddec54ca23
danigonlinea <danigonlinea@gmail.com>
2021-02-17 21:33:45 +0100

    theme added

---
+++ src/styles/themes.js
@@ -0,0 +1,34 @@
+export const themes = {
+  light: {
+    primary: '#ffc759',
+    primaryVariant: '#FDD835',
+    secondary: '#C2185B',
+    text: {
+      default: '#121212',
+      muted: '#666666',
+      highlight: '#58a6ff',
+      success: '#4CAF50',
+      error: '#FF5722',
+      info: '#2196F3',
+      warning: '#FF9800',
+    },
+    background: '#FFFFFF',
+    backgroundVariant: '#F9F9F9',
+  },
+  dark: {
+    primary: '#484f58',
+    primaryVariant: '#010409',
+    secondary: '#ff7b9c',
+    text: {
+      default: '#c9d1d9',
+      muted: '#8b949e',
+      highlight: '#58a6ff',
+      success: '#4CAF50',
+      error: '#FF5722',
+      info: '#2196F3',
+      warning: '#FF9800',
+    },
+    background: '#0d1117',
+    backgroundVariant: '#21262d',
+  },
+}

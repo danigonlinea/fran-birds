e943e9c482a04baad420130cdafd78061921440a
danigonlinea <danigonlinea@gmail.com>
2021-02-28 16:48:25 +0100

    husky lint-staged

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,6 +1,7 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
 import global from './global';
 
+const a = () => {};
 export default function* rootSagas() {
   yield all([global()]);
 }
--- src/styles/styled.js
+++ src/styles/styled.js
@@ -1,7 +1,6 @@
 import { View } from 'react-native';
 import styled, { css } from 'styled-components/native';
 
-const a = 12;
 export const sContainer = styled(View)`
   flex: 1;
   background-color: '#fff';

b3b04d777fa5c61831a5691f32333df156ee74fb
danigonlinea <danigonlinea@gmail.com>
2021-02-28 16:44:52 +0100

    husky lint-staged

--- src/styles/styled.js
+++ src/styles/styled.js
@@ -1,6 +1,7 @@
 import { View } from 'react-native';
 import styled, { css } from 'styled-components/native';
 
+const a = 12;
 export const sContainer = styled(View)`
   flex: 1;
   background-color: '#fff';

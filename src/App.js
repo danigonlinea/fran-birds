a871ad25caf6dfa649d468e2c876309eb8641bcf
danigonlinea <danigonlinea@gmail.com>
2021-02-28 19:36:11 +0100

    adding google auth

--- src/App.js
+++ src/App.js
@@ -4,22 +4,56 @@ import { Provider } from 'react-redux';
 import { StyleSheet, Text, View } from 'react-native';
 import store from './store';
 
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import firebase from 'firebase';
+import { Button } from 'react-native';
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+	firebase.initializeApp({
+		// web initialization
+	});
+}
+
+WebBrowser.maybeCompleteAuthSession();
+
 export default function App() {
-  return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Starting App!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </Provider>
-  );
+	const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+		clientId: ''
+	});
+
+	React.useEffect(() => {
+		if (response?.type === 'success') {
+			const { id_token } = response.params;
+
+			const credential = firebase.auth.GoogleAuthProvider.credential(id_token);
+			firebase.auth().signInWithCredential(credential);
+		}
+	}, [response]);
+
+	return (
+		<Provider store={store}>
+			<View style={styles.container}>
+				<Text>Starting App!</Text>
+				<Button
+					disabled={!request}
+					title="Login"
+					onPress={() => {
+						promptAsync();
+					}}
+				/>
+				<StatusBar style="auto" />
+			</View>
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
